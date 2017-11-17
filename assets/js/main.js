16a545a92e7408609771e338ff9723cf79475ef6
Bruno Lima <bhml@cesar.org.br>
2017-11-17 08:30:35 -0300

    Site v0.5

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var scrollOptions = {
+  mode: 'set',
+  sectionScroll: false,
+  initialScroll: true,
+  keepHistory: false,
+  sectionWrapperSelector: '.section-wrapper',
+  sectionClass: 'section-wrapper__section',
+  animationSpeed: 500,
+  headerHash: 'header',
+  breakpoint: null,
+  eventEmitter: null,
+  dynamicHeight: true
+};
+
+(function () {
+  'use strict';
+
+  // $.smartscroll(scrollOptions);
+
+})();

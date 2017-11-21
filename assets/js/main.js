c4ba5d9cd34467b223a99be4590022d2943540ff
Bruno Lima <bhml@cesar.org.br>
2017-11-21 12:50:17 -0300

    Navigation fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,14 @@
-var scrollOptions = {
-  mode: 'set',
-  sectionScroll: false,
-  initialScroll: true,
-  keepHistory: false,
-  sectionWrapperSelector: '.section-wrapper',
-  sectionClass: 'section-wrapper__section',
-  animationSpeed: 500,
-  headerHash: 'header',
-  breakpoint: null,
-  eventEmitter: null,
-  dynamicHeight: true
-};
-
 (function () {
   'use strict';
 
-  // $.smartscroll(scrollOptions);
+  $('nav a').click(function() {
+    var target = $(this).attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 2000, 'easeOutCubic');
+
+    return false;
+  });
 
 })();
